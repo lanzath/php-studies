16fdb81f51a0ccfd46a6d04aea4339f3c7f34f75
Lanza <lanzathm@gmail.com>
2020-06-01 20:50:35 -0300

    :pencil: PHP Operators

---
+++ Basics/operators.php
@@ -0,0 +1,65 @@
+<!-- PHP Operators -->
+<?php
+
+/**
+ * Arithmetic Operators
+ * Addition +
+ * Subtraction - 
+ * Multiplication *
+ * Division /
+ * Modulus %
+ * Exponentiation 
+ */
+$x = 2;
+$y = 7;
+
+// Addition +
+print_r($x + $y); // 9
+
+// Subtraction -
+print_r($y - $x); // 5
+
+// Multiplication *
+print_r($x * $y); // 14
+
+// Division /
+print_r($y / $x); // 3.5 
+
+// Modulus %
+print_r($y % $x); // 1
+
+// Exponentiation
+print_r($x ** $y); // 128
+
+/**
+ * Comparison Operators
+ * == 	Equal
+ * === 	Identical
+ * != 	Not equal
+ * !== 	Not identical
+ * > 	Greater than
+ * < 	Less than
+ * >= 	Greater than or equal to
+ * <= 	Less than or equal to
+ */
+
+ /**
+  * Increment / Decrement Operators
+  * ++$x 	Pre-increment
+  * $x++ 	Post-increment
+  * --$x 	Pre-decrement
+  * $x-- 	Post-decrement
+  */
+
+  /**
+   * Logical Operators
+   * && and
+   * || or
+   * ! not 
+   */
+
+   /**
+    * For more examples and logical operators
+    * https://www.php.net/manual/pt_BR/language.operators.logical.php
+    * https://www.w3schools.com/php/php_operators.asp
+    */
